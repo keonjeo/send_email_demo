53bf4c6fb2f85a78188158eb73ff83b9eaffaf5d
Keon Ye <jnuyeyizhou@gmail.com>
2018-01-06 15:48:53 +0800

    modify the email_sender when using the 163 email to send the email to customer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,7 +14,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'hello@tetraceratops.com'
+  config.mailer_sender = 'tetraceratops@163.com'
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
